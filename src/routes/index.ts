43174871be59d85846517b0bd02b5efcd9052606
Emmanuel Paul Carrillo Carpio <enmanuelpaulcarrillo.rakata900@gmail.com>
2023-03-25 19:17:10 -0500

    commit_code

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,22 +1,22 @@
-import { Router } from "express";
-import { readdirSync } from "fs"
+// import { Router } from "express";
+// import { readdirSync } from "fs"
 
-const PATH_ROUTER = `${__dirname}`
-const router = Router();
+// const PATH_ROUTER = `${__dirname}`
+// const router = Router();
 
-const clearFileName = (fileName: string) =>{
-    const file = fileName.split('.').shift()
-    return file
-}
+// const clearFileName = (fileName: string) =>{
+//     const file = fileName.split('.').shift()
+//     return file
+// }
 
-readdirSync(PATH_ROUTER).filter((filename)=>{
-    const cleanName = clearFileName(filename)
-    if(cleanName !== 'index'){
-        import (`./${cleanName}`).then((moduleRouter)=>{
-            router.use(`/${cleanName}`,moduleRouter.router)
-        })
-    }
+// readdirSync(PATH_ROUTER).filter((filename)=>{
+//     const cleanName = clearFileName(filename)
+//     if(cleanName !== 'index'){
+//         import (`./${cleanName}`).then((moduleRouter)=>{
+//             router.use(`/${cleanName}`,moduleRouter.router)
+//         })
+//     }
     
-})
+// })
 
-export{ router }
+// export{ router }
